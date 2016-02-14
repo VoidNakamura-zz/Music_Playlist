9df0cd8f0453c66ca5ff9fb30b9d540528acb031
Jimmy <nerdherdx212@gmail.com>
2016-02-14 16:39:02 -0500

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,4 +1,7 @@
-var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-console.log("You just visited " + "  " + firstHref);
+var url = "https://www.youtube.com/watch?v=57cOmxB12XI";
+var clientID = url.substr(url.indexOf("=")).slice(0);
+while(clientID.charAt(0) === '=') {
+    clientID = clientID.substr(1);
+    console.log(clientID);
+}
 
