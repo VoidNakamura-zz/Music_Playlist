377b620d15e884e0c24de851e90014659a60c55a
Nerdherdx <nerdherdx212@gmail.com>
2016-02-14 17:29:01 -0500

    Added form for input

--- js/content.js
+++ js/content.js
@@ -1,10 +1,14 @@
- $("#add").on("click",function(){
-      $("#url").text();
+$(document).ready(function(){
+    $("#add").click(function(e){
+     var url = $(e.target).text();
+        console.log("aaaaaaaaaaaa");
+
      var clientID = url.substr(url.indexOf("=")).slice(0);
      while(clientID.charAt(0) === '=') {
-         clientID = clientID.substr(1);
-         console.log(clientID);
-    }
+        clientID = clientID.substr(1);
+         console.log("aaaa");
+     }
+    });
 
 });
 
