e8108d154f6e4a2ce008f34dfca99811c7e0a0bf
Jimmy <nerdherdx212@gmail.com>
2016-02-14 16:54:19 -0500

    Update content.js

--- js/content.js
+++ js/content.js
@@ -1,7 +1,15 @@
-var url = "https://www.youtube.com/watch?v=57cOmxB12XI";
-var clientID = url.substr(url.indexOf("=")).slice(0);
-while(clientID.charAt(0) === '=') {
-    clientID = clientID.substr(1);
-    console.log(clientID);
-}
+ $("#add").on("click",function(){
+      $("#url").text();
+     var clientID = url.substr(url.indexOf("=")).slice(0);
+     while(clientID.charAt(0) === '=') {
+         clientID = clientID.substr(1);
+         console.log(clientID);
+    }
+
+});
+
+
+
+
+
 
